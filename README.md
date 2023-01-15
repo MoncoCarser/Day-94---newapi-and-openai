eb27a284a066e07e95a14487f30a96ba04fcf6a9
Pasi P <pjpakkala@gmail.com>
2023-01-15 12:59:36 +0000

    Final version. openai.organization was updated to openai.organisation and token amount was raised to 50.  These seem to have been main issues preventing proper results from printing earlier. Code was improved by checking the solution code and asking Chat GPT also check the code.
    Comments added to help future me better remember how this worked.
    Unneeded file news.py was removed.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-In thsi repl I learn about using 2 different APIs together.
+In this repl I learn about using 2 different APIs together.
 An API for news and openai to for example summarize the news.
--- main.py
+++ main.py
@@ -1,42 +1,34 @@
-import requests, json, os
-import openai
+import os, requests, json, openai
 
+#Replit's secrets contain the necessary keys received from newsapi and openai web pages
 news_api = os.environ['news_api_key']
-country = "us"
-url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={news_api}"
-#url = f"https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey={news_api}"
-
-openai.organization = os.environ['openai_organization_id']
+openai.organisation = os.environ['openai_organization_id']
 openai.api_key = os.environ['openai_api_key']
+#Model.list() returns information about all models available
 openai.Model.list()
 
-
-
+country = "fr" #we can choose what country gb / us / etc.
+url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={news_api}"
 
 result = requests.get(url)
 data = result.json()
-#print(json.dumps(data, indent = 2))
 
-"""for article in data["articles"]:
-    print(article["title"])
-    print()
-    print(article["url"])
-    print()
-    print(article["content"])
-    print()
-"""
+# If there is an error in the API request, the code will exit
+if result.status_code != 200:
+    print("Failed to retrieve news articles. Error code: " + str(result.status_code))
+    exit()
+
+#if needed to print the news stories:
+#print(data["articles"][0]["content"])
+
+#below loops through 5 news, asks openai to summarize them, and returns the one sentence version
+counter = 0
 for article in data["articles"]:
-    content =  article["content"]
-    prompt = f"Summarize following in one sentence: {content}"
-    
-    response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=6)
-
-    print(response["choices"][0]["text"].strip())    
+    counter +=1
+    if counter > 5:
+        break
+    #promt is the key to modify our request, like when asking Chat GPT to do something
+    prompt = (f"Summarize {article['title']}, {article['content']} in one sentence.")
+    response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0, max_tokens=50)
+    print(response["choices"][0]["text"].strip())
     print()
-
-#print(response)
-
-
-#1 modify newsapi code to get desired data 
-#2 give the data to openai to summarzie
-#3 get 5 news stories printed this way
